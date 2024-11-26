161578235ea725fde03e55d7530aaea307e3b248
shinji530 <shinji53059@gmail.com>
2024-11-26 21:51:43 +0900

    Feat: 오류 수정

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,15 +1,17 @@
+import { BadRequestError } from "../errors/index.js";
+
 export default function formatDate(date) {
     const format = /^\d{4}\/\d{2}\/\d{2}$/;
 
     if (!format.test(date)) {
-        throw new Error("Invalid date format");
+        throw new BadRequestError("날짜 형식이 올바르지 않습니다.");
     }
 
     const [year, month, day] = date.split("/").map(Number);
     
     const parsedDate = new Date(year, month - 1, day);
     if (parsedDate.getFullYear() !== year || parsedDate.getMonth() !== month - 1 || parsedDate.getDate() !== day) {
-        throw new Error("Invalid date");
+        throw new BadRequestError("유효하지 않은 날짜입니다.");
     }
     
     return parsedDate;
